a759c1c1ce92ed91f591409118898810d2238520
quanghuy <huytran.301099@gmail.com>
2020-09-10 00:55:17 +0700

    - final action update, delete

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
-var listSV = [];
+var listSV = [{
+    MSSV: 102,
+    Name: 'Huy',
+    Email: 'huytran.301099@gmail.com'
+}];
+
+function check_MSSV(MSSV){
+    for(i=0;i<listSV.length;i++){
+        if(listSV[i].MSSV == MSSV)
+            return true;
+    }
+    return false;
+}
 function myFunction(){
     let MSSV = document.getElementById("MSSV").value
     let Name = document.getElementById("Name").value
     let Email = document.getElementById("Email").value
-    listSV.push({
-        ID: listSV.length + 1,
-        MSSV: MSSV,
-        Name: Name,
-        Email: Email
-    })
+    if(check_MSSV(parseInt(MSSV)) === false) {
+        if (_.isEmpty(MSSV) || _.isEmpty(Name) || _.isEmpty(Email)){
+            if(_.isEmpty(MSSV)){
+                document.getElementById('MSSV').style.borderColor = "red"
+            }
+            else{
+                document.getElementById('MSSV').style.borderColor = "green"
+            } 
+            if(_.isEmpty(Name)){
+                document.getElementById('Name').style.borderColor = "red"
+            }
+            if(_.isEmpty(Email)){
+                document.getElementById('Email').style.borderColor = "red"
+            }
+        }
+        else{
+            listSV.push({
+                MSSV: parseInt(MSSV),
+                Name: Name,
+                Email: Email
+            })
+        }
+    }
+    else {
+        listSV.map((sv,index) => {
+            if(sv.MSSV == parseInt(MSSV))
+               sv.Name = Name;
+               sv.Email = Email;
+        });
+        title = document.getElementById('title').innerHTML = 'Thêm sinh viên'
+    }
     viewDSSV();
 }
 
+function find_SV(ID){
+    listSV.map((sv,index) => {
+        if(sv.MSSV == parseInt(ID))
+           result = sv;
+    });
+    return result;
+}
+
 function update(ID){
-    console.log(ID);
+    SinhVien = find_SV(ID);
+    title = document.getElementById('title').innerHTML = 'Sửa sinh viên'
+    document.getElementById("MSSV").value = SinhVien.MSSV;
+    document.getElementById("Name").value = SinhVien.Name;
+    document.getElementById("Email").value = SinhVien.Email;
 }
 
 function deleteSV(ID){
-    console.log(ID);
+    for( var i = 0; i < listSV.length; i++){ 
+        if ( listSV[i].MSSV === parseInt(ID)) {
+          listSV.splice(i, 1); 
+        }
+     }
+    viewDSSV();
 }
 
 function viewDSSV(){
@@ -28,8 +82,8 @@ function viewDSSV(){
                     <th>${SV.Name}</th>
                     <th>${SV.Email}</th>
                     <th>
-                        <button type="button" class="btn btn-warning" onclick='update(${SV.ID})'>Update</button>
-                        <button type="button" class="btn btn-danger" onclick='deleteSV(${SV.ID})'>Delete</button>
+                        <button type="button" class="btn btn-warning" onclick='update(${SV.MSSV})'>Update</button>
+                        <button type="button" class="btn btn-danger" onclick='deleteSV(${SV.MSSV})'>Delete</button>
                     </th>
                 </tr>`
     })
